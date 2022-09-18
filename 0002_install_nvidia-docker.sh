134ae71b70549ba588af47cbbdf9f08cc4010022
FUJun Ubuntu PC <fujunde@gmail.com>
2022-09-18 18:37:48 +0200

    nvidia docker

--- 0002_install_nvidia-docker.sh
+++ 0002_install_nvidia-docker.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-#sudo apt update && sudo apt -y install nvidia-container-toolkit
+ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+         
 apt update && apt -y install nvidia-docker2
 systemctl daemon-reload
 systemctl restart docker
---
+++ 0016_pytorch_opencv_pycharm_panda3d_pyqt_mediapipe/0001_build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t pytorch/pytorch:mediapipe -f 0000_Dockerfile .
